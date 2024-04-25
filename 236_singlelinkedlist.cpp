bd7105f21ecae510f8b77bb7a20cd43eb0adba8f
akmaalsyh <taufansyah50@gmail.com>
2024-04-25 16:27:32 +0700

    memasukkan try switch case 1 dan 2

--- 236_singlelinkedlist.cpp
+++ 236_singlelinkedlist.cpp
@@ -128,6 +128,29 @@ void searchData() {
 int main() {
     int pilihan;
     do {
-        
+        try {
+            cout << "1. Tambah Data " << endl;
+            cout << "2. Hapus Data " << endl;
+            cout << "3. Tampilkan Data " << endl;
+            cout << "4. Cari Data " << endl;
+            cout << "5. Keluar " << endl;
+            cout << "Pilihan: ";
+            cin >> pilihan;
+            switch (pilihan) {
+                case 1:
+                    addNode();
+                    cout << "Data Berhasil Ditambahkan" << endl;
+                    system("pause");
+                    system("cls");
+                    break;
+                case 2:
+                    if (listempty()) {
+                        cout << "List Kosong" << endl;
+                        system("pause");
+                        system("cls");
+                        break;
+                    }
+            }
+        }
     }
 }
